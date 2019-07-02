c249b6c878676a661082f2a386d9a61810282ede
daramfon <Daramfon@buffalo.edu>
2019-07-01 20:47:41 -0400

    new new

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        //Code I would have used if I was working with frames and not auto layout.
+//        swoosh.frame  = CGRect(x: view.frame.width / 2 - swoosh.frame.width/2, y: 50, width: swoosh.frame.width, height: swoosh.frame.height)
+//        bgImg.frame  = view.frame
+    } 
 
 
 }
